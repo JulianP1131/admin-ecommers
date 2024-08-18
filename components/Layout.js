541c81383e2d158a734128ddc35bbc5a78c79c30
JulianP <julprogram131@gmail.com>
2024-08-18 02:19:36 -0500

    Actualizacion de estilos y logo en login

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,23 @@
-// Importaciones
+// components/Layout.js
 import Nav from "@/components/Nav";
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn } from "next-auth/react";
 
-export default function Layout({children}) {
-  const { data: session } = useSession()
+export default function Layout({ children }) {
+  const { data: session } = useSession();
 
   if (!session) {
     return (
-      <div className="bg-rojoE w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button onClick={() => signIn('google')} className="bg-white p2 px-4 rounded-lg">Login con Google</button>
+      <div className="layout-container">
+        <div className="login-container">
+          <img src="/img/Logo.png" className="logo" alt="Logo" />
+          <button onClick={() => signIn('google')} className="login-button">
+            Login con Google
+          </button>
         </div>
       </div>
-    )
+    );
   }
-  
+
   return (
     <div className="bg-vinoT min-h-screen flex">
       <Nav />
